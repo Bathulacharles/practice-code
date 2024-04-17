4bdd47adf38b4a500f2c48ed4555c9d664515cea
Charles Bathula <charles.bathula22@gmail.com>
2024-04-17 15:01:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import './App.css'
 //import styles from './appStyles.module.css';
 //import Form from './components/Form';
 import pureComponent from './components/pureComponent';
+//import pureComponent from './components/pureComponent';
 
 
 function App() {
